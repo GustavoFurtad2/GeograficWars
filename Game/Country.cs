eb9b8f2d4d57f0bffbc9d3b1bbfaac56cbcfc9fd
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2025-12-03 22:34:37 -0300

    chore: add json property for every attribute

--- Game/Country.cs
+++ Game/Country.cs
@@ -1,16 +1,33 @@
-﻿namespace GeograficWars.Game
+﻿using System.Text.Json.Serialization;
+
+namespace GeograficWars.Game
 {
     public class Country
     {
+        [JsonPropertyName("id")]
+        public string Id { get; private set; }
+
+        [JsonPropertyName("displayname")]
         public string Name { get; private set; }
+
+        [JsonPropertyName("continent")]
         public string Continent { get; private set; }
+
+        [JsonPropertyName("path")]
         public string ImagePath { get; private set; }
+
+        [JsonPropertyName("x")]
         public double X { get; private set; }
+
+        [JsonPropertyName("y")]
         public double Y { get; private set; }
+
+        [JsonPropertyName("scale")]
         public double Scale { get; private set; }
 
-        public Country(string name, string continent, string imagePath, double x, double y, double scale)
+        public Country(string id, string name, string continent, string imagePath, double x, double y, double scale)
         {
+            Id = id;
             Name = name;
             Continent = continent;
             ImagePath = imagePath;
