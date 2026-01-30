d4ce567520b620bc93f330643d347e0756faf433
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2026-01-30 00:49:15 -0300

    feat: implement game state

---
+++ Game/GameState.cs
@@ -0,0 +1,10 @@
+﻿namespace GeograficWars.Game
+{
+    public class GameState
+    {
+        public Dictionary<string, Player> Players { get; } = new();
+        public Dictionary<string, Country> Countries { get; } = new();
+
+        public string? CurrentPlayerTurnId { get; set; }
+    }
+}
