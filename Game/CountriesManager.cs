3551a25d7ade5ce8bf7f51054814d94a2822b280
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2026-01-30 00:52:03 -0300

    feat: implement new countries manager

---
+++ Game/CountriesManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+
+namespace GeograficWars.Game
+{
+    public class CountriesManager
+    {
+        private readonly GameState _state;
+
+        public CountriesManager(GameState state)
+        {
+            _state = state;
+        }
+
+        public Country CreateCountry(Country country)
+        {
+            _state.Countries[country.CountryId] = country;
+
+            return country;
+        }
+
+        public void RemoveCountry(string countryId)
+        {
+            if (_state.Countries[countryId] != null)
+            {
+                _state.Countries.Remove(countryId);
+            }
+        }
+
+        public void LoadCountries()
+        {
+            string json = File.ReadAllText("wwwroot/countries.json");
+
+            var countriesList = JsonSerializer.Deserialize<List<CountryData>>(json);
+
+            foreach (var countryData in countriesList)
+            {
+                Country country = new Country(countryData);
+
+                _state.Countries[country.CountryId] = country;
+            }
+        }
+
+        public List<Country> GetCountries()
+        {
+            List<Country> countries = _state.Countries.Values.ToList();
+
+            return countries;
+        }
+    }
+}
