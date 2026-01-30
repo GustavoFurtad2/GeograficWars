7e62b90fafffbd231a10340bb90706aed687859c
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2026-01-30 00:47:49 -0300

    feat: implement initia player class

---
+++ Game/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace GeograficWars.Game
+{
+    public class Player
+    {
+        public string Id { get; private set; } = Guid.NewGuid().ToString();
+        public string Name { get; private set; } = string.Empty;
+        public bool Alive { get; private set; } = false;
+        public int ConqueredTerritories { get; private set; } = 0;
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
