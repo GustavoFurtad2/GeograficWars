dc108348228fdc7f24affa7405e472513e806103
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2026-01-30 00:50:32 -0300

    feat: implement basic players manager

---
+++ Game/PlayersManager.cs
@@ -0,0 +1,29 @@
+﻿namespace GeograficWars.Game
+{
+    public class PlayersManager
+    {
+        private readonly GameState _state;
+
+        public PlayersManager(GameState state)
+        {
+            _state = state;
+        }
+
+        public Player CreatePlayer(string name)
+        {
+            Player player = new Player(name);
+
+            _state.Players[player.Id] = player;
+
+            return player;
+        }
+
+        public void RemovePlayer(string id)
+        {
+            if (_state.Players[id] != null)
+            {
+                _state.Players.Remove(id);
+            }
+        }
+    }
+}
