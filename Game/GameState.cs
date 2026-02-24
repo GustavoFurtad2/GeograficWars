0e103d8f66a8e7124e46da3901e41cf47af8d416
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2026-02-24 02:24:10 -0300

    feat: shuffle the order of the players

--- Game/GameState.cs
+++ Game/GameState.cs
@@ -1,10 +1,21 @@
-﻿namespace GeograficWars.Game
+﻿using GeograficWars.Shared;
+
+namespace GeograficWars.Game
 {
     public class GameState
     {
         public Dictionary<string, Player> Players { get; } = new();
         public Dictionary<string, Country> Countries { get; } = new();
 
+        public List<string> PlayersOrderID { get; private set; } = new();
+
         public string? CurrentPlayerTurnId { get; set; }
+
+        public void newGame()
+        {
+            PlayersOrderID = Players.Keys.ToList();
+
+            Shuffler.Shuffle(PlayersOrderID);
+        }
     }
 }
