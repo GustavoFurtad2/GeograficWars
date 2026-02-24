d352d172ee7a752090a75af6ab0cd041c5f7dae3
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2026-02-24 02:23:48 -0300

    feat: shuffler module

---
+++ Shared/Shuffler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeograficWars.Shared
+{
+    public static class Shuffler
+    {
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(List<T> list)
+        {
+            int n = list.Count;
+
+            while (n > 1)
+            {
+                n--;
+
+                int k = rng.Next(n + 1);
+
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
